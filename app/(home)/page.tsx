ac97d99e6d074ce334293e8edb39ec18ab0a9599
GustavoYM01 <gumatimoto@hotmail.com>
2024-02-02 15:53:12 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,9 +2,31 @@ import Image from "next/image";
 import Header from "../_components/header";
 
 export default function Home() {
+  const showDate = () => {
+    const date = new Date()
+      .toLocaleString("pt-BR", {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+      })
+      .replace("-feira", "");
+    const nameOfWeek =
+      date.split(",")[0].substring(1, -1).toUpperCase() +
+      date.split(",")[0].substring(1);
+    const nameOfMonth =
+      date.split("de")[1].substring(1, 2).toUpperCase() +
+      date.split("de")[1].substring(2);
+    return `${nameOfWeek}, ${new Date().getDate()} de ${nameOfMonth}`;
+  };
   return (
     <div>
       <Header />
+      <div className="px-5 py-5">
+        <h2 className="text-xl">
+          Olá, <b>Miguel!</b>
+        </h2>
+        <span className="text-sm">{showDate()}</span>
+      </div>
     </div>
   );
 }

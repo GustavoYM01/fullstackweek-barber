131f1224002c33072d5fd1d6eaa9d9a007d0cfbd
GustavoYM01 <gumatimoto@hotmail.com>
2024-02-02 18:24:32 -0300

    feat: add initial booking component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Header from "../_components/header";
 import Search from "./_components/search";
+import BookingItem from "../_components/booking-item";
 
 export default function Home() {
   const showDate = () => {
@@ -30,6 +31,13 @@ export default function Home() {
       </div>
       <div className="px-5 mt-6">
         <Search />
+      </div>
+
+      <div className="px-5 mt-6">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">
+          Agendamentos
+        </h2>
+        <BookingItem />
       </div>
     </div>
   );

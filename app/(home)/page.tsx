51d73ff1b29a65ddc3b74d3c0fa2f5ca8d33f6ed
GustavoYM01 <gumatimoto@hotmail.com>
2024-02-02 14:45:07 -0300

    feat: add initial header component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import Header from "../_components/header";
 
 export default function Home() {
   return (
-    <h1>TESTE</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
---
+++ app/_components/header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className="p-5 flex justify-between items-center">
+        <Image src="/Logo.png" alt="FSW Barber" height={22} width={120} />
+        <Button variant={"outline"} size={"icon"} className="h-8 w-8">
+          <MenuIcon size={16} />
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Header;

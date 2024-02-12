fcaa8ddd6d38affcec695cbfa4b038baf2a2ef04
GustavoYM01 <gumatimoto@hotmail.com>
2024-02-12 11:14:07 -0300

    chore: change names env

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,8 +8,8 @@ const handler = NextAuth({
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.PUBLIC_GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.PUBLIC_GOOGLE_CLIENT_SECRET as string,
     }),
   ],
 });
